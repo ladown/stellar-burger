0d94c91099145cdc5edd91faaede7aed91c3f68e
Egor Ermilov <egorka33rus52@gmail.com>
2024-07-21 15:59:19 +0300

    Create e2e tests for modal with ingredients

---
+++ cypress/e2e/ingredient/ingredient-modal.cy.ts
@@ -0,0 +1,20 @@
+describe('Проверяем работу модального окна', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4000');
+    cy.get('[data-cy="ingradient-modal-trigger"]').first().click();
+  });
+
+  it('Модальное окно должно открываться по клику на ссылку в карточке ингредиента', () => {
+    cy.get('[data-cy="ingradient-modal"]').should('exist');
+  });
+
+  it('Модальное окно должно закрываться по клику на крестик', () => {
+    cy.get('[data-cy="modal-close"]').click();
+    cy.get('[data-cy="ingradient-modal"]').should('not.exist');
+  });
+
+  it('Модальное окно должно закрываться по клику на overlay', () => {
+    cy.get('[data-cy="modal-overlay"]').click(0, 0);
+    cy.get('[data-cy="ingradient-modal"]').should('not.exist');
+  });
+});
--- src/components/ui/burger-ingredient/burger-ingredient.tsx
+++ src/components/ui/burger-ingredient/burger-ingredient.tsx
@@ -21,6 +21,7 @@ export const BurgerIngredientUI: FC<TBurgerIngredientUIProps> = memo(
           className={styles.article}
           to={`/ingredients/${_id}`}
           state={locationState}
+          data-cy='ingradient-modal-trigger'
         >
           {count && <Counter count={count} />}
           <img className={styles.img} src={image} alt='картинка ингредиента.' />
