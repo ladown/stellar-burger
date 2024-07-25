8c9c19db4bc614a7f836457712e28bd8e6c53201
Egor Ermilov <egorka33rus52@gmail.com>
2024-07-25 23:01:50 +0300

    Initialize jest

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@ui(.*)$': '<rootDir>/src/components/ui$1',
+    '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^@utils-types(.*)$': '<rootDir>/src/utils/types$1',
+    '^@api(.*)$': '<rootDir>/src/utils/burger-api.ts',
+    '^@services(.*)$': '<rootDir>/src/services$1',
+    '^@slices(.*)$': '<rootDir>/src/services/slices$1',
+    '^@store(.*)$': '<rootDir>/src/services/store.ts',
+    '^@hooks(.*)$': '<rootDir>/src/hooks$1'
+  }
+};
+
+export default config;
