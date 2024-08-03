d10afe867ba7b3038576ae07031310a92c180dd7
Egor Ermilov <egorka33rus52@gmail.com>
2024-08-03 14:43:21 +0300

    Add accessToken to cookies

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,4 +1,9 @@
 beforeEach(() => {
+  cy.setCookie(
+    'accessToken',
+    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Njc1MzVkOTdlZGUwMDAxZDA2ZmQ3ZSIsImlhdCI6MTcyMjY3NTkzMCwiZXhwIjoxNzIyNjc3MTMwfQ.4mkg-YCmfK64eCZ7CTdOkdpnfziRf5DEJb9_0DPIo9s'
+  );
+
   cy.visit('/');
 
   cy.intercept('GET', '/api/ingredients', {
@@ -12,4 +17,8 @@ beforeEach(() => {
   cy.intercept('POST', '/api/orders', {
     fixture: 'orders.json'
   });
+});
+
+afterEach(() => {
+  cy.clearCookie('accessToken');
 });
