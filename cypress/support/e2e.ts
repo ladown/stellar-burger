e88949d8ca9a5a5d6f3c849635b1eb42d3838e4c
Egor Ermilov <egorka33rus52@gmail.com>
2024-08-03 14:49:43 +0300

    Remove cookies

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,4 @@
 beforeEach(() => {
-  cy.setCookie(
-    'accessToken',
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Njc1MzVkOTdlZGUwMDAxZDA2ZmQ3ZSIsImlhdCI6MTcyMjY3NTkzMCwiZXhwIjoxNzIyNjc3MTMwfQ.4mkg-YCmfK64eCZ7CTdOkdpnfziRf5DEJb9_0DPIo9s'
-  );
-
   cy.visit('/');
 
   cy.intercept('GET', '/api/ingredients', {
@@ -17,8 +12,4 @@ beforeEach(() => {
   cy.intercept('POST', '/api/orders', {
     fixture: 'orders.json'
   });
-});
-
-afterEach(() => {
-  cy.clearCookie('accessToken');
 });
