5972c116b04c91f73251dae9c4a78fb57a2739a7
Egor Ermilov <egorka33rus52@gmail.com>
2024-08-04 11:04:46 +0300

    Add HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './components/app/app';
@@ -12,9 +12,10 @@ const root = createRoot(container!);
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      {/* Its needed to use HashRouter to hav proper routing in GitHubPages or in subdomains */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
